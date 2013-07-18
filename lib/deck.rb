38e08af8b59b66a11b6e8b02d24c683757252282
Caleb Ott <ottboy4@gmail.com>
2013-07-18 08:16:36 -0600

    started adding logic for scoring hand

--- lib/deck.rb
+++ lib/deck.rb
@@ -18,4 +18,29 @@ class Deck
       @cards.push(Card.new(i, suit))
     end
   end
+
+  def score_cards(cards)
+
+  end
+
+  def is_straight?(cards)
+    new_cards = cards.sort { |x,y| x.number < y.number}
+
+  end
+
+  def is_flush?(cards)
+
+  end
+
+end
+
+module Values
+  STRAIGHT_FLUSH = 1000
+  FOUR_OF_A_KIND = 900
+  FULL_HOUSE = 800
+  FLUSH = 700
+  STRAIGHT = 600
+  THREE_OF_A_KIND = 500
+  TWO_PAIR = 400
+  ONE_PAIR = 300
 end
